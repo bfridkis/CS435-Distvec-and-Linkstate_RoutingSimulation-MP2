578bf66d2f6af1ab9d81f8c379f425770b384870
bfridkis <bfridkis@gmail.com>
2024-03-09 10:44:42 -0600

    fixed brackets

--- SOURCES/messagePrint.cpp
+++ SOURCES/messagePrint.cpp
@@ -101,34 +101,35 @@ void messagePrint(std::vector<std::map<int, std::multimap<int, std::vector<int>>
         std::string line, message;
         getline(messages, line);
 		//std::cout << "Line in messagePrint: " << line << "Line is Empty Check: " << line.empty() << std::endl;
-        if((!line.empty()) && (std::find_if_not(line.begin(),line.end(),[](char c){return std::isspace(c)};) != line.end()))
-		int sourceNode, destNode, cost;
-        std::istringstream messageStr(line);
-		//std::cout << "messageStr: " << messageStr << std::endl;
-        messageStr >> sourceNode >> destNode;
-        getline(messageStr, message);
-        if(_FT[sourceNode].find(destNode) != _FT[sourceNode].end()) {
-			cost = _FT[sourceNode].find(destNode)->second.begin()->first;
-			std::vector<int> lowestCostPath(_FT[sourceNode].find(destNode)->second.begin()->second);
-			if (lowestCostPath.size() == 1) {
-				_outFile << "from " << sourceNode << " to " << destNode << " cost " << cost << " hops " << sourceNode;
-			}
-			else if (lowestCostPath.size() > 1) {
-				_outFile << "from " << sourceNode << " to " << destNode << " cost " << cost << " hops " << sourceNode << " ";
+        if((!line.empty()) && (std::find_if_not(line.begin(),line.end(),[](char c){return std::isspace(c)};) != line.end())) {
+			int sourceNode, destNode, cost;
+			std::istringstream messageStr(line);
+			//std::cout << "messageStr: " << messageStr << std::endl;
+			messageStr >> sourceNode >> destNode;
+			getline(messageStr, message);
+			if(_FT[sourceNode].find(destNode) != _FT[sourceNode].end()) {
+				cost = _FT[sourceNode].find(destNode)->second.begin()->first;
 				std::vector<int> lowestCostPath(_FT[sourceNode].find(destNode)->second.begin()->second);
-				for(std::vector<int>::iterator it = lowestCostPath.begin(); it != lowestCostPath.end()-1; it++) {
-					if(it == lowestCostPath.end()-2) {
-						_outFile << *it;
-					}
-					else {
-						_outFile << *it << " ";
+				if (lowestCostPath.size() == 1) {
+					_outFile << "from " << sourceNode << " to " << destNode << " cost " << cost << " hops " << sourceNode;
+				}
+				else if (lowestCostPath.size() > 1) {
+					_outFile << "from " << sourceNode << " to " << destNode << " cost " << cost << " hops " << sourceNode << " ";
+					std::vector<int> lowestCostPath(_FT[sourceNode].find(destNode)->second.begin()->second);
+					for(std::vector<int>::iterator it = lowestCostPath.begin(); it != lowestCostPath.end()-1; it++) {
+						if(it == lowestCostPath.end()-2) {
+							_outFile << *it;
+						}
+						else {
+							_outFile << *it << " ";
+						}
 					}
 				}
+				_outFile << " message" << message << std::endl;
 			}
-			_outFile << " message" << message << std::endl;
-		}
-		else {
-			_outFile << "from " << sourceNode << " to " << destNode << " cost infinite hops unreachable message" << message << std::endl;
+			else {
+				_outFile << "from " << sourceNode << " to " << destNode << " cost infinite hops unreachable message" << message << std::endl;
+			}
 		}
     }   
     _outFile << std::endl;
