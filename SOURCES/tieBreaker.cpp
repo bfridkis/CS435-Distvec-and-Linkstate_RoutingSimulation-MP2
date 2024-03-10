2f4a6a6fd8eaca61a0a29c0d6ec31159234051d9
bfridkis <bfridkis@gmail.com>
2024-03-10 16:06:42 -0500

    troubleshooting

--- SOURCES/tieBreaker.cpp
+++ SOURCES/tieBreaker.cpp
@@ -9,8 +9,8 @@
 
 //Tie breaker function. Next hop lowest node number wins the tie. Returns 1 if pathA is winner, 0 if pathB is winner, and -1 if paths are identical. Will not handle paths that are identical for the entirety of one path but of dissimilar lenghts. (However, these should never be passed as they should never have the same cost.)
 int tieBreaker(std::vector<int> pathA, std::vector<int> pathB) {
-	if(pathA.size() == 1) { return 1 }
-	if(pathB.size() == 1) { return 0 }
+	if(pathA.size() == 1) { return 1; }
+	if(pathB.size() == 1) { return 0; }
 	std::vector<int>::iterator aIt, bIt;
 	for(aIt = pathA.begin(), bIt = pathB.begin(); aIt != pathA.end() && bIt != pathB.end(); aIt++, bIt++) {
 			//std::cout << "new path node: " << *aIt << " existing path node: " << *bIt << std::endl;
