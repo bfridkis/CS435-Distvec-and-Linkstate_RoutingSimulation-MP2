ecee9e77079e307a39ed809602864753fadac321
bfridkis <bfridkis@gmail.com>
2024-03-10 20:15:45 -0500

    additional changes for overloaded converge

--- SOURCES/processChanges.cpp
+++ SOURCES/processChanges.cpp
@@ -102,14 +102,15 @@ void processChanges(std::vector<std::map<int, std::multimap<int, std::vector<int
         //Reconverge for each node after changes applied (Run twice to ensure all lowest paths are found.)
         for (int sourceNode = 1; sourceNode < _FT.size(); sourceNode++) {
             std::cout << "converging for node: << " << sourceNode << " (after change " << j << ")" << std::endl;
-            converge(sourceNode, -1, -1, _FT);
+            converge(FT, i, i, nullptr, 0);
+			//converge(sourceNode, -1, -1, _FT);
             //converge(sourceNode, -1, -1, _FT, nullptr);
         }
-		for (int sourceNode = _FT.size()-1; sourceNode > 0; sourceNode--) {
+		/*for (int sourceNode = _FT.size()-1; sourceNode > 0; sourceNode--) {
             std::cout << "converging for node: << " << sourceNode << " (after change " << j << ")" << std::endl;
             converge(sourceNode, -1, -1, _FT);
             //converge(sourceNode, -1, -1, _FT, nullptr);
-        }
+        }*/
         std::cout << std::endl;
         std::cout << "Forwarding tables after change " << j++ << " and subsequent reconvergence applied..." << std::endl;
         consoleOutFT(_FT);
