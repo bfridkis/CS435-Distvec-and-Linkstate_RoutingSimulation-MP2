204d99e6c89786bcbe9a9b29ac6813ef6669e948
bfridkis <bfridkis@gmail.com>
2024-03-10 19:10:20 -0500

    adding a new, overloaded converge function

--- SOURCES/converge.cpp
+++ SOURCES/converge.cpp
@@ -361,7 +361,7 @@ void converge(std::vector<std::map<int, std::multimap<int, std::vector<int>>>> &
 			for(auto&& [reachableNodeCost, reachableNodePath] : reachableNodePaths) {
 				//if node is neighbor (i.e. directly connected) and this path is lowest cost option of direct links (in case multiple direct links of different costs)
 				//if(reachableNodePath.size() == 1 && reachableNodePath == *(_FT[prevNode].find(reachableNode)->second.find(reachableNodeCost)->begin())) {
-				if(reachableNodePath.size() == 1) {	
+				if(reachableNodePath.size() == 1 && newPath.find(reachableNode) == newPath.end()) {
 					newPathCost += reachableNodeCost;
 					//If there is not yet a path established from source to this node...(will never have to do this for a node's first level of recursion because initial topology loads all direct links into FT)
 					if(_FT[sourceNode].find(reachableNode) == _FT[sourceNode].end() || _FT[sourceNode].find(reachableNode)->second.begin()->first > newPathCost) {
