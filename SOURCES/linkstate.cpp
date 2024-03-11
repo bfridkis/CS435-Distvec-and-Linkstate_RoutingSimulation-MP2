0c2987f0f63b239c3c49cdaef286b48f2845d7ca
bfridkis <bfridkis@gmail.com>
2024-03-11 16:12:20 -0500

    fixing errors with linkstate

--- SOURCES/linkstate.cpp
+++ SOURCES/linkstate.cpp
@@ -137,7 +137,7 @@ int main(int argc, char** argv) {
     std::cout << std::endl;
     
     //Initial Routing Table Convergences. (Needs to run twice to capture any lower cost options established later in convergence for lower numbered nodes.)
-    for (int sourceNode = 1; i < FT.size(); sourceNode++) {
+    for (int sourceNode = 1; sourceNode < FT.size(); sourceNode++) {
         std::cout << std::endl;
         std::cout << "Initial convergence (1) for node: " << i << std::endl;
         converge(sourceNode, TT, FT);
