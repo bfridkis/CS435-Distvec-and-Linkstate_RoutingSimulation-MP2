ceb5ad7712129851a295896ff6836c3c1c4a23b4
bfridkis <bfridkis@gmail.com>
2024-03-10 22:17:03 -0500

    additional changes for overloaded converge

--- SOURCES/converge.cpp
+++ SOURCES/converge.cpp
@@ -356,11 +356,11 @@ void converge(std::vector<std::map<int, std::multimap<int, std::vector<int>>>> &
 	//}
 	//For node directly connected to the source node, recursively find paths to all other reachable nodes. (sourceNode == prevNode on first level of recursion)
 	for(auto&& [reachableNode, reachableNodePaths] : _FT[prevNode]) {
+		newPath->push_back(prevNode);
 		//Ignore source self entry
 		if(reachableNode != sourceNode) {
 			for(auto&& [reachableNodeCost, reachableNodePath] : reachableNodePaths) {
 				//if node is neighbor (i.e. directly connected) and this path is lowest cost option of direct links (in case multiple direct links of different costs)
-				newPath->push_back(prevNode);
 				if(reachableNodePath.size() == 1 && std::find(newPath->begin(), newPath->end(), reachableNode) == newPath->end()) {
 					newPathCost += reachableNodeCost;
 					std::cout << "New Converge... sourceNode: " << sourceNode << " prevNode: " << prevNode << " reachableNode: " << reachableNode << " reachableNodeCost: " << reachableNodeCost << " newPath: " << vecToString(*newPath) << " newPathCost: " << newPathCost << std::endl;
