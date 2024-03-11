619b66eaf6da70e26e0047daa492c878364f3245
bfridkis <bfridkis@gmail.com>
2024-03-11 01:11:45 -0500

    additional changes for overloaded converge

--- SOURCES/tieBreaker.cpp
+++ SOURCES/tieBreaker.cpp
@@ -11,6 +11,7 @@
 int tieBreaker(std::vector<int> pathA, std::vector<int> pathB) {
 	//if(pathA.size() == 1) { return 1; }
 	//if(pathB.size() == 1) { return 0; }
+	std::cout << "Path's to compare in tieBreaker: Path A - " << vecToString(pathA) << " Path B - " << vecToString(pathB) << std::endl;
 	std::vector<int>::iterator aIt, bIt;
 	for(aIt = pathA.begin(), bIt = pathB.begin(); aIt != pathA.end() && bIt != pathB.end(); aIt++, bIt++) {
 			//std::cout << "new path node: " << *aIt << " existing path node: " << *bIt << std::endl;
