f05ad875d611c49419d2854b0752ce9811e148da
bfridkis <bfridkis@gmail.com>
2024-03-11 17:08:31 -0500

    fixing errors with linkstate

--- SOURCES/converge.cpp
+++ SOURCES/converge.cpp
@@ -460,9 +460,9 @@ void converge(int sourceNode, std::vector<std::map<int, int>> &_TT, std::vector<
 	}
 	
 	//First iteration, for all of sourceNode's directly connected nodes
-	for(auto&& [reachableNode, reachableNode_nexthop_cost] : _TT[sourceNode]) {
+	for(std::map<int, int>::iterator it = _TT[sourceNode].begin(); it != _TT[sourceNode].end(); it++) {
 		if(reachableNode != sourceNode) {
-			int reachableNodeNextHop = reachableNode_nexthop_cost.first, reachableNodeCost = reachableNode_nexthop_cost.second;
+			int reachableNodeNextHop = it->first, reachableNodeCost = it->second;
 			//Add cost to dijkstras entry from source node to this reachable node
 			dijk.find(reachableNode)->second.first = reachableNodeNextHop;
 			dijk.find(reachableNode)->second.second = reachableNodeCost;
