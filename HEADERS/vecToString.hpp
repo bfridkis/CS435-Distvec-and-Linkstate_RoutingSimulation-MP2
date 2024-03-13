ab930a8bb76d35735740ff521fe5edaf5a39742b
bfridkis <bfridkis@gmail.com>
2024-03-13 15:41:44 -0500

    troubleshooting/fixing...

--- HEADERS/vecToString.hpp
+++ HEADERS/vecToString.hpp
@@ -10,7 +10,6 @@
 
 #include "include.hpp"
 
-	std::string vecToString(std::vector<int>& vec, std::string sep = std::string(" "));
-//std::string setToString(std::set<int>& set);
-std::string setToString(std::set<int>& set, std::string sep = std::string(" "));
+std::string vecToString(std::vector<int>& vec, const char *sep);
+std::string setToString(std::set<int>& set, const char *sep);
 #endif
