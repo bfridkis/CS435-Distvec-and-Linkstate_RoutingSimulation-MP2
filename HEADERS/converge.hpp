48fc7201832c85629a2928aa48c9fe7bba96f5f9
bfridkis <bfridkis@gmail.com>
2024-03-12 23:23:44 -0500

    adding path tracking (for tie breaking)

--- HEADERS/converge.hpp
+++ HEADERS/converge.hpp
@@ -13,6 +13,6 @@
 void converge(int sourceNode, int neighbor, int prevHop, std::vector<std::map<int, std::multimap<int, std::vector<int>>>> &_FT);
 void converge(std::vector<std::map<int, std::multimap<int, std::vector<int>>>> &_FT, int sourceNode, int prevNode, std::vector<int>* newPath, int newPathCost);
 //void converge(int sourceNode, std::vector<std::map<int, int>> &_TT, std::vector<std::map<int, std::pair<int, int>>> &_FT);
-void converge(int sourceNode, std::vector<std::map<int, int>> &_TT, std::vector<std::map<int, std::pair<vector<int>, int>>> &_FT, std::vector<std::pair<int,int>> &tiesTracker) {	
+void converge(int sourceNode, std::vector<std::map<int, int>> &_TT, std::vector<std::map<int, std::pair<std::vector<int>, int>>> &_FT, std::vector<std::pair<int,int>> &tiesTracker) {	
 
 #endif
