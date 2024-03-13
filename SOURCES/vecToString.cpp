3a0fd16536e2466ed7f22f31b0fe40de2f6a9dc4
bfridkis <bfridkis@gmail.com>
2024-03-12 23:55:24 -0500

    adding path tracking (for tie breaking)

--- SOURCES/vecToString.cpp
+++ SOURCES/vecToString.cpp
@@ -7,7 +7,7 @@
 
 #include "../HEADERS/vecToString.hpp"
 
-std::string vecToString(std::vector<int>& vec, std::string = std::string("")) {
+std::string vecToString(std::vector<int>& vec, const char *sep = "") {
     if (vec.size() > 0) { 
         std::stringstream pathSS;
         std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(pathSS, sep));
@@ -18,7 +18,7 @@ std::string vecToString(std::vector<int>& vec, std::string = std::string("")) {
         return retStr; }
 }
 
-std::string setToString(std::set<int>& set, std::string = std::string("")) {
+std::string setToString(std::set<int>& set, const char *sep = "") {
     if (set.size() > 0) { 
         std::stringstream pathSS;
         std::copy(set.begin(), set.end(), std::ostream_iterator<int>(pathSS, sep));
